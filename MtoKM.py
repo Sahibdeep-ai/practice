230e1001e0e42189bad0cdcb736f582fa87df483
Sahibdeep-ai <sahibdeepsingh179@gmail.com>
2025-10-19 01:21:36 -0700

    miles into km

---
+++ MtoKM.py
@@ -0,0 +1,8 @@
+#write a program to convert Miles into Kilometers
+# 1KM = 0.621 miles
+
+miles=0.621
+a=int(input("Enter Miles you want to conert in kilometers:"))
+res=a*miles
+print(a,"Miles into km is:",res)
+
