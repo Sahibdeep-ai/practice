f2c0f9b4843e41157e11d3c00b2e69a32a377007
Sahibdeep-ai <sahibdeepsingh179@gmail.com>
2025-10-19 01:50:34 -0700

    practice

---
+++ prac2.py
@@ -0,0 +1,10 @@
+# wap  to input three numbers swap them as; 2nd no. gets the value 1st + 2nd and 3rd no. gets the vlue of 2nd + 3rd.
+
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+num3=int(input("Enter number 3:"))
+num2=num1+num2
+num3= num2+num3
+print("Num 1=",num1)
+print("Num 2=",num2)
+print("Num 3=",num3)
